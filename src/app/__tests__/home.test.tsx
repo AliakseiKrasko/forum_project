20459348a783a26784b3f5c05a2e6e7f70f4c0ce
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-08-09 12:23:55 +0300

    chore(scripts): add test and e2e for CI

--- src/app/__tests__/home.test.tsx
+++ src/app/__tests__/home.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {screen} from "@testing-library/react";
 import Home from "@/app/page";
+import {renderWithProviders} from "../../../test/utils";
 
 test("renders filter select", () => {
-    render(<Home />);
+    renderWithProviders(<Home />);
     expect(screen.getByText(/Все пользователи/)).toBeInTheDocument();
 });
---
+++ test/utils.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
+
+export function renderWithProviders(ui: ReactElement) {
+    return render(<Provider store={store}>{ui}</Provider>);
+}
